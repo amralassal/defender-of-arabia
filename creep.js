2977b0d2d3594fcb9d3c2eebf7863cdc8e940a97
Mazen Wagdy <mazenwagdy01@gmail.com>
2011-08-09 09:19:12 +0200

    ctest

--- creep.js
+++ creep.js
@@ -1,18 +1,6 @@
 //vsr x
 var Creep = Class.create(Unit, {
-	parent : "creep",
-	speeds : [0, 1.08, 2.245, 4.852, 6.023, 7.945, 11.71, 22.625],
-	angles : [0, 3.75, 7.5, 15, 18, 22.5, 30, 45],
-	cannonTheta : 0,
-	olderTheta : 0, oldestTheta: 0,
-	hp : 100, maxHp : 100,
-	speed :4, price : 4,
-	evading : false, direction : 0,
-	rate : 0.1, power: 1.0,
-	cannonDisplacement : [-4, 0],
-	turningPoint : [0, 0],
-	range : 1,
-	
+	// destest
 	initialize : function($super,x,y,scene, extension){
 		$super(x,y,scene,extension)
 		Map.grid[x][y].push(this)
